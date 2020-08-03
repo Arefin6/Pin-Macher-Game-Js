18ec668f635db30e217001d4b9bc828d2e1b0073
Arefi6 <arefinhossain3@gmail.com>
2020-08-03 14:58:46 +0600

    first Part added

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert("Hello pin");
+document.getElementById("generate-btn").addEventListener("click",function(){
+
+  let random = Math.floor(1000 + Math.random() * 9000);
+  document.getElementById("random-input").value = random;
+  
+});
